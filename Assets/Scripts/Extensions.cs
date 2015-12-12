e18914f04af8906ad2faebaef8c63a50401332c0
Gleb Ostrowski <glebos@web.de>
2015-12-12 01:59:17 +0100

    Update Readme and Add Extensions
    Extensions methods, to be precises. The handy little static methods to
    expend class functionality.

--- Assets/Scripts/Extensions.cs
+++ Assets/Scripts/Extensions.cs
@@ -1,15 +1,35 @@
+using System;
+using System.Linq;
+
 namespace Goost
 {
-    public class Extensions
+    public static class Extensions
     {
-        // Use this for initialization
-        private void Start()
+        public static bool IsIn<T>(this T source, params T[] list)
         {
+            if (null == source) throw new ArgumentNullException("source");
+            return list.Contains(source);
         }
 
-        // Update is called once per frame
-        private void Update()
+        // Enable quick and more natural string.Format calls
+        public static string F(this string s, params object[] args)
         {
+            return string.Format(s, args);
+        }
+
+        public static bool CoinToss(this Random rng)
+        {
+            return rng.Next(2) == 0;
+        }
+
+        public static T OneOf<T>(this Random rng, params T[] things)
+        {
+            return things[rng.Next(things.Length)];
+        }
+
+        public static bool Between<T>(this T actual, T lower, T upper) where T : IComparable<T>
+        {
+            return actual.CompareTo(lower) >= 0 && actual.CompareTo(upper) < 0;
         }
     }
 }
--- README.md
+++ README.md
@@ -1,9 +1,3 @@
-# Innocent Cherry LD 33 Jam Entry
+# Project Peragus LD 34 Jam Entry
 
-  - Took me around 24h-48h, I was working on and off for three days
-  - I was packing and stuff (last weekend old room), therefore no straightforward time...
-  - Annoying Bug: Sometimes, if going right first, one get stucks in red screen. Only restart helps.
-  	- This does not happen, when going left first and see the end.
-  	- Somebody knows why?  
-
-Code is licensed under CC0.
+Everything is possible.
